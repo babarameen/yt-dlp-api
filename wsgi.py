7867d2a39a3809197f5a20625e5bb1de03f2cf78
babarameen <48354924+babarameen@users.noreply.github.com>
2025-07-06 17:23:02 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
 from index import app
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway provides PORT env
-    print(f"🚀 Starting Flask on 0.0.0.0:{port}")
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run()
